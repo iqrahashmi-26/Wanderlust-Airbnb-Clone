3e9f302e508978b65f6dd47abd1bc92ddc703660
iqrahashmi-26 <iqrahashmi2005@gmail.com>
2026-01-24 22:33:17 +0530

    Add Changes

--- app.js
+++ app.js
@@ -57,12 +57,12 @@ const sessionOptions = {
     store,
     secret:process.env.SECRET,
     resave: false,
-    saveUninitialized:false,
+    saveUninitialized:true,
     cookie:{
         httpOnly: true,
         expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
     }
 };
 
--- controllers/listings.js
+++ controllers/listings.js
@@ -1,6 +1,5 @@
 const Listing = require("../models/listing.js");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const { search } = require("../routes/listing.js");
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
